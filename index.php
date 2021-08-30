cf3974401beffce1b3fabcd53fa5b95ec011b11a
yungshun317 <seraph317@gmail.com>
2021-08-30 13:50:37 +0800

    [FEAT] Create the `index.php` file

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Lamp Blog | Home</title>
+</head>
+<body>
+    <!-- container -->
+    <div class="container">
+        <!-- navbar -->
+        <div class="navbar">
+            <div class="logo-div">
+                <a href="index.php"><h1>Lamp Blog</h1></a>
+            </div>
+            <ul>
+                <li><a class="active" href="index.php"></a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </div>
+
+        <!-- Page Layout -->
+        <div class="content">
+            <h2 class="content-title">Recent Articles</h2>
+            <hr>
+        </div>
+
+        <!-- footer -->
+        <div class="footer">
+            <p>ChouQin Info Co. &copy; <?php echo date('Y'); ?></p>
+        </div>
+    </div>
+</body>
+</html>
