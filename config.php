8459bd79617f51bd41fcf1da1ca0ca4412e49492
yungshun317 <seraph317@gmail.com>
2021-09-02 14:04:44 +0800

    [CHORE] Return a database connectivity object `$conn` which we can use in the application for querying the database

--- config.php
+++ config.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+// Connect to database
+$conn = mysqli_connect("localhost", "yungshun", "lucifer317", "lamp_blog");
+
+if (!$conn) {
+    die("Error connecting to database: " . mysqli_connect_error());
+}
+
+// Define global constants
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 define('BASE_URL', 'http://localhost/lamp-blog/');
 ?>
