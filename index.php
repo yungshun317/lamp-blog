4dce44f05ee2f8485c7325864f6c61622d935609
yungshun317 <seraph317@gmail.com>
2021-09-04 10:47:27 +0800

    [FEAT] Display posts in `index.php`

--- index.php
+++ index.php
@@ -28,6 +28,21 @@
         <div class="content">
             <h2 class="content-title">Recent Articles</h2>
             <hr>
+
+            <?php foreach ($posts as $post): ?>
+                <div class="post" style="margin-left: 0px;">
+                    <img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+                    <a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+                        <div class="post_info">
+                            <h3><?php echo $post['title'] ?></h3>
+                            <div class="info">
+                                <span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+                                <span class="read_more">Read more...</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach ?>
         </div>
 
         <!-- footer -->
