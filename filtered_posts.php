f05946a887c98ba325ba96e16843651e34e18c4d
yungshun317 <seraph317@gmail.com>
2021-09-08 11:17:39 +0800

    [FEAT] Display posts in `filtered_posts.php`

--- filtered_posts.php
+++ filtered_posts.php
@@ -11,7 +11,27 @@
     <title>Blog | Home</title>
 </head>
 <body>
-<div class="container"></div>
-<div class="content"></div>
-
+<div class="container">
+    <!-- Navbar -->
+    <?php include( ROOT_PATH . '/includes/navbar.php'); ?>
+    <div class="content">
+        <h2 class="content-title">
+            Articles on <u><?php echo getTopicNameById($topic_id); ?></u>
+        </h2>
+        <?php foreach ($posts as $post): ?>
+            <div class="post" style="margin-left: 0px;">
+                <img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+                <a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+                    <div class="post_info">
+                        <h3><?php echo $post['title'] ?></h3>
+                        <div class="info">
+                            <span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+                            <span class="read_more">Read more...</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        <?php endforeach ?>
+    </div>
+</div>
 <?php include( ROOT_PATH . '/includes/footer.php'); ?>
